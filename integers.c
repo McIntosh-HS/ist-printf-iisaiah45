013669e7c24178d75f24bce581e815935babede4
isaiah <2027simeon.isaiah@fcboe.org>
2024-01-17 14:47:55 -0500

    Finished

---
+++ integers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>	// The stdio.h library contains input and output functions
+
+int main()
+{
+	int a = 10;
+
+	// Format specifiers are placeholders for variable data
+	printf("a displayed as base 10 interger: %o\n", a);	// %i is for integers
+	printf("a displayed as base 8 interger: %x\n", a);	// %i is for integers
+	printf("a displayed as base 16 interger: %i\n", a);	// %i is for integers
+	printf("a displayed as base 16 interger: %i\n", a);	// %i is for integers
+	printf("a displayed as base 10 unsigned interger: %i\n", a);	// %i is for integers
+
+	return 0;
+}
